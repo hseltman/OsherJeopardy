18c80e6427644e639c6c30858e3a577a8eb76c6c
hseltman <howard.seltman@gmail.com>
2023-10-21 14:27:21 -0400

    Clean up global.R

--- global.R
+++ global.R
@@ -1,9 +1,4 @@
-# sjCateg = c("1940s", "WWII", "Interesting Pittsburgh",
-#             "Mars", "Stravinsky", "Railroads")
-# 
-# djCateg = c("In 'Utero'", "Word Origins", "Mushrooms",
-#             "Country of the Sport", "Dreaded Opera Category", "Plants")
-# 
+# OsherJeopardy global.R file
 
 # For file input
 wd = getwd()
