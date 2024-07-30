27578d75a1426afea2425c07377428d353331883
hseltman <howard.seltman@gmail.com>
2024-07-30 13:30:06 -0400

    Moved helper functions to lookupHelpers.R Coded tally() and part of search()

--- lookup.R
+++ lookup.R
@@ -10,6 +10,7 @@
 
 lookup<- function(loadCurrent=FALSE, loadUsed=FALSE) {
   require(stringr)
+  source("lookupHelpers.R")
   
   # Load or create the name the data.frame
   if (file.exists(file.path("gameboards", "gameboards.RData"))) {
@@ -24,140 +25,6 @@ lookup<- function(loadCurrent=FALSE, loadUsed=FALSE) {
                       stringsAsFactors=FALSE)
   }
   
-  # Main helper function  
-  textWrapper <- function(txt, width=13, length=3, pad=TRUE) {
-    if (substring(txt, 1, 1) == " ") {
-      txt <- substring(txt, 2)
-    }
-    words <- strsplit(txt, " ", fixed=TRUE)[[1]]
-    ncw <- nchar(words)
-    if (length(words)==1 && ncw>width) {
-      words <- c(substring(words, 1, width), substring(words, width+1))
-    }
-    words <- substring(words, 1, width)
-    if (length(words) == 1) {
-      rslt <- c(str_pad(words, width, side="right"), rep(str_pad("", width, side="right"), length-1))
-      return(rslt)
-    }
-    lengths <- nchar(words)
-    cLengths <- cumsum(lengths) + 1
-    cLengths[1] <- cLengths[1] - 1
-    rslt <- rep("", length)
-    for (line in 1:length) {
-      wlw <- cLengths <= width
-      if (any(wlw)) {
-        n = max(which(wlw))
-      } else {
-        n <- 1
-      }
-      rslt[line] <- paste(words[1:n], collapse=" ")
-      if (length(words) == n) break
-      words <- words[-(1:n)]
-      lengths <- nchar(words)
-      cLengths <- cumsum(lengths) + 1
-    }
-    if (pad) rslt <- str_pad(rslt, width, side="right")
-    return(rslt)
-  }
-
-  # Read a gameboard file
-  readOne <- function(fName) {
-    AQSeparator = "|"
-    inData <- try(readLines(fName))
-      if (is(inData, "try-error")) return("Bad File")
-    
-    
-    # Delete blank lines and comment lines
-    inData = inData[!grepl("(^\\s*$)|(^\\s*#)", inData)]
-    
-    # Guess that user totally forgot data format
-    if (length(inData) < 12) {
-      return(paste0("File must contain 12 sets of 6 lines each ",
-                    "(where line 1 is a Category Name, and lines ",
-                    "2 to 5 are 'answer", AQSeparator, "question' pairs), ",
-                    "followed by a Final Jeopardy Category, and then a Final ",
-                    "Jeopardy 'answer", AQSeparator, "question' pair."))
-    } 
-    
-    # Find which lines have a single bar (or 'AQSeparator') 
-    singleBar = grepl(paste0("^([^", AQSeparator, "]+?)[", AQSeparator,
-                             "]([^", AQSeparator, "]+)$"), inData)
-    
-    # Handle incorrect pattern
-    if (length(singleBar) != length(sbPattern) ||  any(singleBar!=sbPattern)) {
-      ## Report on various problems if not successful ##
-      
-      # Handle first part of pattern not 'FTTTTTF'
-      if (singleBar[1]) {
-        return(paste0("First line of game file must contain a category name ",
-                      "(no '", AQSeparator, "')."))
-      }
-      if (!all(singleBar[2:6])) {
-        return(paste0("Lines 2 to 6 of game file must contain 'answer",
-                      AQSeparator, "question' pairs."))
-      }
-      if (singleBar[7]) {
-        return(paste0("Line 7 of game file must contain a category name (no '",
-                      AQSeparator, "')."))
-      }
-      
-      # Use run length encoding to characterize pattern of categories and A|Q pairs
-      catAQPair = rle(singleBar) 
-      temp = catAQPair$lengths[catAQPair$values==FALSE]
-      if (length(temp) != 13) {
-        return(paste("File must contain 6 Jeopardy Categories, 6 Double",
-                     "Jeopardy Categories, and one Final Jeopardy Category.",
-                     "You have", length(temp), "categories."))
-      }
-      if (any(temp != 1)) {
-        index = which(temp != 1)[1]
-        return(paste0("It appears that the '", AQSeparator,
-                      "' is missing in 'Answer", AQSeparator, "Question' for",
-                      " category number ", index, "."))
-      }
-      temp = catAQPair$lengths[catAQPair$values==TRUE]
-      if (length(temp) != 13) {
-        return(paste0("File must contain 6 groups of 5 'Answer", AQSeparator,
-                      "Question' pairs ",
-                      "for Jeopardy, 6 groups of 5 'Answer", AQSeparator,
-                      "Question' pairs for Double Jeopardy, and one Final ",
-                      "Jeopardy 'Answer", AQSeparator, "Question' pair ",
-                      "for a total of 61 pairs.  ",
-                      "You have", sum(temp), "pairs in ", length(temp), "groups."))
-      }
-      if (temp[13] != 1) {
-        return(paste0("There should be just one Final Jeopardy 'Answer",
-                      AQSeparator, "Question' pair."))
-      }
-      if (any(temp[1:12] != 5)) {
-        index = which(temp != 5)[1]
-        return(paste0("Category ", index, " has ", temp[index], " 'Answer",
-                      AQSeparator, "Question' pairs."))
-      }
-      return("Bad input", "Unhandled exception", type="error")
-    } else {
-      ## Input is all goood
-      sjCatLoc = seq(1, by=6, length.out=6)
-      sjCategories = inData[sjCatLoc]
-      djCatLoc = seq(37, by=6, length.out=6)
-      djCategories = inData[djCatLoc]
-      fjCategory = inData[73]
-      tc = textConnection(inData[1:36][-sjCatLoc])
-      sjAQ = read.table(tc, sep=AQSeparator, quote="",
-                        col.names=c("Answer", "Question"), comment.char="")
-      close(tc)
-      tc = textConnection(inData[37:72][-sjCatLoc]) # [sic]
-      djAQ = read.table(tc, sep=AQSeparator, quote="",
-                        col.names=c("Answer", "Question"), comment.char="")
-      close(tc)
-      temp = strsplit(inData[74], AQSeparator, fixed=TRUE)[[1]]
-      fjAQ = data.frame(Answer=temp[1], Question=temp[2])
-    }    
-    return(list(fName=fName, sjCategories=sjCategories,
-                djCategories=djCategories, fjCategory=fjCategory,
-                sjAQ=sjAQ, djAQ=djAQ, fjAQ=fjAQ))
-  }    
-
   # Define category types
   categoryTypes <- c("geography", "history", "music", "art", "literature",
                       "science", "technology", "wordplay", "languages",
@@ -165,53 +32,9 @@ lookup<- function(loadCurrent=FALSE, loadUsed=FALSE) {
                       "people", "sports", "culinary", "transportation",
                       "medicine", "media", "games", "other", "quit")
   
-  # Input a category type by partial matching from the list of categoryTypes
-  getACategoryType <- function(category) {
-    index <- NA
-    while (TRUE) {
-      input <- readline(paste0(category, "? "))
-      index <- pmatch(input, categoryTypes)
-      if (!is.na(index)) break
-      cat(paste(categoryTypes, collapse=", "), "\n")
-    }
-    return(categoryTypes[index])
-  }
-  
-  # load one gameboard with categories
-  loadAndCategorize <- function(fName) {
-    boardVec <- c(rep("s", 30), rep("d", 30), "f")
-    one = readOne(fName)
-    if (!is.list(one)) {
-      cat(one, "\n")
-      return(NULL)
-    }
-    myCats <- character(0)
-    for (category in one$sjCategories) {
-      ct <- getACategoryType(category)
-      if (ct == "quit") return(NULL)
-      myCats <- c(myCats, rep(ct, 5))
-    }
-    for (category in one$djCategories) {
-      ct <- getACategoryType(category)
-      if (ct == "quit") return(NULL)
-      myCats <- c(myCats, rep(ct, 5))
-    }
-    ct <- getACategoryType(one$fjCategory)
-    if (ct == "quit") return(NULL)
-    myCats <- c(myCats, ct)
-    temp <- data.frame(filename=basename(fName),
-                       board=boardVec,
-                       category=c(rep(one$sjCategories, each=5),
-                                  rep(one$djCategories, each=5),
-                                  one$fjCategory),
-                       categoryType=myCats,
-                       answer=c(one$sjAQ$Answer, one$djAQ$Answer, one$fjAQ$Answer),
-                       question=c(one$sjAQ$Question, one$djAQ$Question, one$fjAQ$Question))
-    dtf <- rbind(dtf, temp)
-  }
   
   newGames <- FALSE
-  # Load any current gameboards
+  # Load any current game boards
   if (loadCurrent) {
     onFile <- unique(dtf$filename)
     files <- grep("20.*[.]txt", list.files("gameboards"), value=TRUE)
@@ -228,7 +51,7 @@ lookup<- function(loadCurrent=FALSE, loadUsed=FALSE) {
     }
   }
 
-    # Load any used gameboards
+    # Load any used game boards
   if (loadUsed) {
     onFile <- unique(dtf$filename)
     fp <- paste0("gameboards", .Platform$file.sep, "used")
@@ -246,12 +69,38 @@ lookup<- function(loadCurrent=FALSE, loadUsed=FALSE) {
     }
   }
   
+  # Re-save the data if any new games were added
   if (any(newGames)) {
     save(dtf, file=file.path("gameboards", "gameboards.RData"))
   }
   
   # Query the data 
-  browser()
+  while(TRUE) {
+    #browser()
+    cat("quit/tally/c/t/a/q/aq SEARCH TERMS [|short/narrow]\n")
+    text <- readline("? ")
+    text <- strsplit(text, "|", fixed=TRUE)[[1]]
+    short <- FALSE
+    narrow <- FALSE
+    if (length(text) > 1) {
+      if (grepl("short", text[2])) short <- TRUE
+      if (grepl("narrow", text[2])) narrow <- TRUE
+      text <- text[1]
+    } 
+    text <- strsplit(text, " ", fixed=TRUE)[[1]]
+    if (!text[1] %in% c("quit", "tally", "c", "t", "a", "q", "aq")) {
+      cat("Bad input\n")
+      next
+    }
+    if (text[1] == "quit") break
+    if (text[1] == "tally") {
+      tally(dtf)
+      next
+    }
+    searchTerms <- paste(text[-1], collapse=" ")
+    text <- text[1]
+    search(text, searchTerms, dtf, short, narrow)    
+  }
   
   # 
   return(invisible(NULL))
---
+++ lookupHelpers.R
@@ -0,0 +1,205 @@
+# Helper files for lookup()
+# July 2024
+
+
+# Read a gameboard file
+# Input: a game file name (with path and extension)
+# Output: 
+#   Either 
+#   1) a character with an error message 
+#   or
+#   2) a list with:
+#      fName: character
+#      sjCategories: character vector of length 6
+#      djCategories: character vector of length 6
+#      fjCategory: character
+#      sjAQ: data.frame with 30 rows and columns "Answer" and "Question"
+#      djAQ: data.frame with 30 rows and columns "Answer" and "Question"
+#      fjAQ: data.frame with 1 row and columns "Answer" and "Question"
+#      
+readOne <- function(fName) {
+  AQSeparator = "|"
+  inData <- try(readLines(fName))
+  if (is(inData, "try-error")) return("Bad File")
+  
+  
+  # Delete blank lines and comment lines
+  inData = inData[!grepl("(^\\s*$)|(^\\s*#)", inData)]
+  
+  # Guess that user totally forgot data format
+  if (length(inData) < 12) {
+    return(paste0("File must contain 12 sets of 6 lines each ",
+                  "(where line 1 is a Category Name, and lines ",
+                  "2 to 5 are 'answer", AQSeparator, "question' pairs), ",
+                  "followed by a Final Jeopardy Category, and then a Final ",
+                  "Jeopardy 'answer", AQSeparator, "question' pair."))
+  } 
+  
+  # Find which lines have a single bar (or 'AQSeparator') 
+  singleBar = grepl(paste0("^([^", AQSeparator, "]+?)[", AQSeparator,
+                           "]([^", AQSeparator, "]+)$"), inData)
+  
+  # Handle incorrect pattern
+  if (length(singleBar) != length(sbPattern) ||  any(singleBar!=sbPattern)) {
+    ## Report on various problems if not successful ##
+    
+    # Handle first part of pattern not 'FTTTTTF'
+    if (singleBar[1]) {
+      return(paste0("First line of game file must contain a category name ",
+                    "(no '", AQSeparator, "')."))
+    }
+    if (!all(singleBar[2:6])) {
+      return(paste0("Lines 2 to 6 of game file must contain 'answer",
+                    AQSeparator, "question' pairs."))
+    }
+    if (singleBar[7]) {
+      return(paste0("Line 7 of game file must contain a category name (no '",
+                    AQSeparator, "')."))
+    }
+    
+    # Use run length encoding to characterize pattern of categories and A|Q pairs
+    catAQPair = rle(singleBar) 
+    temp = catAQPair$lengths[catAQPair$values==FALSE]
+    if (length(temp) != 13) {
+      return(paste("File must contain 6 Jeopardy Categories, 6 Double",
+                   "Jeopardy Categories, and one Final Jeopardy Category.",
+                   "You have", length(temp), "categories."))
+    }
+    if (any(temp != 1)) {
+      index = which(temp != 1)[1]
+      return(paste0("It appears that the '", AQSeparator,
+                    "' is missing in 'Answer", AQSeparator, "Question' for",
+                    " category number ", index, "."))
+    }
+    temp = catAQPair$lengths[catAQPair$values==TRUE]
+    if (length(temp) != 13) {
+      return(paste0("File must contain 6 groups of 5 'Answer", AQSeparator,
+                    "Question' pairs ",
+                    "for Jeopardy, 6 groups of 5 'Answer", AQSeparator,
+                    "Question' pairs for Double Jeopardy, and one Final ",
+                    "Jeopardy 'Answer", AQSeparator, "Question' pair ",
+                    "for a total of 61 pairs.  ",
+                    "You have", sum(temp), "pairs in ", length(temp), "groups."))
+    }
+    if (temp[13] != 1) {
+      return(paste0("There should be just one Final Jeopardy 'Answer",
+                    AQSeparator, "Question' pair."))
+    }
+    if (any(temp[1:12] != 5)) {
+      index = which(temp != 5)[1]
+      return(paste0("Category ", index, " has ", temp[index], " 'Answer",
+                    AQSeparator, "Question' pairs."))
+    }
+    return("Bad input", "Unhandled exception", type="error")
+  } else {
+    ## Input is all goood
+    sjCatLoc = seq(1, by=6, length.out=6)
+    sjCategories = inData[sjCatLoc]
+    djCatLoc = seq(37, by=6, length.out=6)
+    djCategories = inData[djCatLoc]
+    fjCategory = inData[73]
+    tc = textConnection(inData[1:36][-sjCatLoc])
+    sjAQ = read.table(tc, sep=AQSeparator, quote="",
+                      col.names=c("Answer", "Question"), comment.char="")
+    close(tc)
+    tc = textConnection(inData[37:72][-sjCatLoc]) # [sic]
+    djAQ = read.table(tc, sep=AQSeparator, quote="",
+                      col.names=c("Answer", "Question"), comment.char="")
+    close(tc)
+    temp = strsplit(inData[74], AQSeparator, fixed=TRUE)[[1]]
+    fjAQ = data.frame(Answer=temp[1], Question=temp[2])
+  }    
+  return(list(fName=fName, sjCategories=sjCategories,
+              djCategories=djCategories, fjCategory=fjCategory,
+              sjAQ=sjAQ, djAQ=djAQ, fjAQ=fjAQ))
+}    
+
+
+# Get a category type from the keyboard by partial matching
+# from the list of categoryTypes
+# Input: the name of a category
+# Output: the category type corresponding to the user's input (may be "quit")
+#
+getACategoryType <- function(category) {
+  index <- NA
+  while (TRUE) {
+    input <- readline(paste0(category, "? "))
+    index <- pmatch(input, categoryTypes)
+    if (!is.na(index)) break
+    cat(paste(categoryTypes, collapse=", "), "\n")
+  }
+  return(categoryTypes[index])
+}
+
+# Load one game board with categories
+# Input: game board file name and path
+# Output: 
+#  NULL if an error
+#  or
+#  a data.frame from the game board (61 x 6)
+#    filename: character
+#    board: character ('s'x30, 'd'x30, 'f')
+#    category: character
+#    categoryType: character
+#    answer: character
+#    question: character
+#    answer: character
+#
+loadAndCategorize <- function(fName) {
+  boardVec <- c(rep("s", 30), rep("d", 30), "f")
+  one = readOne(fName)
+  if (!is.list(one)) {
+    cat(one, "\n")
+    return(NULL)
+  }
+  myCats <- character(0)
+  for (category in one$sjCategories) {
+    ct <- getACategoryType(category)
+    if (ct == "quit") return(NULL)
+    myCats <- c(myCats, rep(ct, 5))
+  }
+  for (category in one$djCategories) {
+    ct <- getACategoryType(category)
+    if (ct == "quit") return(NULL)
+    myCats <- c(myCats, rep(ct, 5))
+  }
+  ct <- getACategoryType(one$fjCategory)
+  if (ct == "quit") return(NULL)
+  myCats <- c(myCats, ct)
+  temp <- data.frame(filename=basename(fName),
+                     board=boardVec,
+                     category=c(rep(one$sjCategories, each=5),
+                                rep(one$djCategories, each=5),
+                                one$fjCategory),
+                     categoryType=myCats,
+                     answer=c(one$sjAQ$Answer, one$djAQ$Answer, one$fjAQ$Answer),
+                     question=c(one$sjAQ$Question, one$djAQ$Question, one$fjAQ$Question))
+  return(temp)
+}
+
+
+# Tally the current data.frame
+#
+tally <- function(dtf) {
+  cat("# games =", length(unique(dtf$filename)), "\n")
+  pie(table(dtf$categoryType), main="Per question")
+  cat("Per board:\n")
+  pb <- dtf[!duplicated(paste(dtf$filename, dtf$board, dtf$category)),
+            "categoryType"]
+  tab <- table(pb)
+  tab <- round(tab/sum(tab)*100)
+  tab <- data.frame(type=names(tab), percent=as.numeric(tab))
+  print(tab, row.names=FALSE)
+  return(NULL)
+}
+
+# Search something
+#
+search <- function(what, searchTerm, dtf, short, narrow) {
+  codes <- c("category", "type", "answer", "question", "aq")
+  what <- codes[pmatch(what, codes)]
+  
+  cat("look in", what, "for", searchTerm, ":", short, narrow, "\n")
+  return(NULL)
+}
+
